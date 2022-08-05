f3b984571854b6036b971024202813177b7ecdb7
sylvielcq <slcq.dev@gmail.com>
2022-08-04 17:51:53 -0700

    linted

--- index.js
+++ index.js
@@ -31,20 +31,20 @@ const { nextISSTimesForMyLocation } = require('./iss');
 ////////////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////////
 
-/** 
- * Input: 
+/**
+ * Input:
  *   Array of data objects defining the next fly-overs of the ISS.
  *   [ { risetime: <number>, duration: <number> }, ... ]
- * Returns: 
+ * Returns:
  *   undefined
- * Sideffect: 
+ * Sideffect:
  *   Console log messages to make that data more human readable.
  *   Example output:
  *   Next pass at Mon Jun 10 2019 20:11:44 GMT-0700 (Pacific Daylight Time) for 468 seconds!
  */
 
 
- const printPassTimes = function(passTimes) {
+const printPassTimes = function(passTimes) {
   for (const pass of passTimes) {
     const datetime = new Date(0);
     datetime.setUTCSeconds(pass.risetime);
--- iss.js
+++ iss.js
@@ -109,15 +109,15 @@ const fetchISSFlyOverTimes = function(coords, callback) {
 /**
  * Orchestrates multiple API requests in order to determine the next 5 upcoming ISS fly overs for the user's current location.
  * Input:
- *   - A callback with an error or results. 
+ *   - A callback with an error or results.
  * Returns (via Callback):
  *   - An error, if any (nullable)
  *   - The fly-over times as an array (null if error):
  *     [ { risetime: <number>, duration: <number> }, ... ]
- */ 
+ */
  
 const nextISSTimesForMyLocation = function(callback) {
-  
+
   fetchMyIP((error, ip) => {
     if (error) {
       callback(error, null);
@@ -139,9 +139,9 @@ const nextISSTimesForMyLocation = function(callback) {
         callback(null, passTimes);
 
       });
-    })
+    });
   });
-}
+};
 
 
 
